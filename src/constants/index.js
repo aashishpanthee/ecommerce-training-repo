7f1e681d007b9761b7375323c8af9cbee21ccb84
Aashish Panthee <aashishpanthee060@gmail.com>
2026-07-01 12:41:17 +0545

    chore: initialize project with Navbar Component

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const navigation = [
+  { name: "Home", href: "#", current: true },
+  { name: "Products", href: "#", current: false },
+  { name: "Teams", href: "#", current: false },
+  { name: "Contact", href: "#", current: false },
+];
