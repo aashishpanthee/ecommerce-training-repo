911e0abe022ec1ab7a6905eea4839451d4f95da8
Aashish Panthee <aashishpanthee060@gmail.com>
2026-07-01 13:45:50 +0545

    feat: add ProductGrid Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ProductGrids from "./components/ProductGrids";
 
 function App() {
   return (
     <section>
       <Navbar />
       <Hero />
+      <ProductGrids />
     </section>
   );
 }
