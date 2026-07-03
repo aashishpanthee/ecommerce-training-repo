18fbf16a642f871ea0b1e505dd9db7355d9b41b8
Aashish Panthee <aashishpanthee060@gmail.com>
2026-07-03 14:12:32 +0545

    fix: add slice method to products in homepage

--- src/components/ProductGrids.jsx
+++ src/components/ProductGrids.jsx
@@ -50,7 +50,7 @@ export default function ProductGrids() {
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">Our Products</h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
+          {products.slice(0, 12).map((product) => (
             <div key={product.id} className="group relative">
               <img
                 alt={product.title}
