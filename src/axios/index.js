d0238839a035c82af6e55e361fe2653460a276a6
Aashish Panthee <aashishpanthee060@gmail.com>
2026-07-03 13:58:57 +0545

    feat: made api call in homepage for products

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+  baseURL: 'https://api.escuelajs.co/api/v1/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
